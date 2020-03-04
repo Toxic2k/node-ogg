1e49494b40d6be3666c8b020570c2149f5387248
mbernardini <marc@valyant.ai>
2020-03-04 16:21:51 -0700

    Update decoder to use native struct bindings

--- src/binding.cc
+++ src/binding.cc
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2020, Valyant AI
  * Copyright (c) 2012, Nathan Rajlich <nathan@tootallnate.net>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -150,7 +151,7 @@ void OggPacket::Init(Napi::Env env, Napi::Object exports)
 Napi::Value OggPacket::packet(const Napi::CallbackInfo &info)
 {
   Napi::Env env = info.Env();
-  return Napi::Number::New(env, op.bytes);
+  return Napi::Buffer<uint8_t>::New(env, op.packet, op.bytes);
 }
 
 void OggPacket::setPacket(const Napi::CallbackInfo &info, const Napi::Value &value)
@@ -267,12 +268,12 @@ void OggSyncState::Init(Napi::Env env, Napi::Object exports)
 {
   Napi::HandleScope scope(env);
 
-  Napi::Function func = DefineClass(env, "OggSyncState", {});
+  Napi::Function func = DefineClass(env, "ogg_sync_state", {});
 
   constructor = Napi::Persistent(func);
   constructor.SuppressDestruct();
 
-  exports.Set("OggSyncState", func);
+  exports.Set("ogg_sync_state", func);
 }
 
 Napi::Object OggSyncState::NewInstance(Napi::Value arg)
@@ -318,46 +319,26 @@ void node_ogg_sync_write(const Napi::CallbackInfo &info)
   (new OggSyncWriteWorker(&syncState->oy, data, cb))->Queue();
 }
 
-// NAN_METHOD(node_ogg_sync_init)
-// {
-//   Nan::HandleScope scope;
-//   ogg_sync_state *oy = reinterpret_cast<ogg_sync_state *>(UnwrapPointer(info[0]));
-//   Local<Value> rtn = Nan::New<Integer>(ogg_sync_init(oy));
-//   info.GetReturnValue().Set(rtn);
-// }
-
 /* combination of "ogg_sync_buffer", "memcpy", and "ogg_sync_wrote" on the thread
  * pool.
  */
-
-// NAN_METHOD(node_ogg_sync_write)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_sync_state *oy = reinterpret_cast<ogg_sync_state *>(UnwrapPointer(info[0]));
-//   char *buffer = reinterpret_cast<char *>(UnwrapPointer(info[1]));
-//   long size = static_cast<long>(info[2]->NumberValue(v8::Isolate::GetCurrent()->GetCurrentContext()).ToChecked());
-//   Nan::Callback *callback = new Nan::Callback(info[3].As<Function>());
-
-//   Nan::AsyncQueueWorker(new OggSyncWriteWorker(oy, buffer, size, callback));
-// }
 
 /* Reads out an `ogg_page` struct. */
 class OggSyncPageoutWorker : public Napi::AsyncWorker
 {
 public:
-  OggSyncPageoutWorker(ogg_sync_state *oy, Napi::Function &callback)
-      : Napi::AsyncWorker(callback), oy(oy), page(), serialno(-1), packets(-1), rtn(0)
+  OggSyncPageoutWorker(ogg_sync_state *oy, ogg_page *page, Napi::Function &callback)
+      : Napi::AsyncWorker(callback), oy(oy), page(page), serialno(-1), packets(-1), rtn(0)
   {
   }
   ~OggSyncPageoutWorker() {}
   void Execute()
   {
-    rtn = ogg_sync_pageout(oy, &page);
+    rtn = ogg_sync_pageout(oy, page);
     if (rtn == 1)
     {
-      serialno = ogg_page_serialno(&page);
-      packets = ogg_page_packets(&page);
+      serialno = ogg_page_serialno(page);
+      packets = ogg_page_packets(page);
     }
   }
   void OnOK()
@@ -369,7 +350,7 @@ public:
 
 private:
   ogg_sync_state *oy;
-  ogg_page page;
+  ogg_page *page;
   int serialno;
   int packets;
   int rtn;
@@ -379,20 +360,10 @@ void node_ogg_sync_pageout(const Napi::CallbackInfo &info)
 {
   OggSyncState *syncState =
       Napi::ObjectWrap<OggSyncState>::Unwrap(info[0].As<Napi::Object>());
-  Napi::Function cb = info[1].As<Napi::Function>();
-  (new OggSyncPageoutWorker(&syncState->oy, cb))->Queue();
+  OggPage *page = Napi::ObjectWrap<OggPage>::Unwrap(info[1].As<Napi::Object>());
+  Napi::Function cb = info[2].As<Napi::Function>();
+  (new OggSyncPageoutWorker(&syncState->oy, &page->op, cb))->Queue();
 }
-
-// NAN_METHOD(node_ogg_sync_pageout)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_sync_state *oy = reinterpret_cast<ogg_sync_state *>(UnwrapPointer(info[0]));
-//   ogg_page *page = reinterpret_cast<ogg_page *>(UnwrapPointer(info[1]));
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(new OggSyncPageoutWorker(oy, page, callback));
-// }
 
 static int serial = 1;
 OggStreamState::OggStreamState(const Napi::CallbackInfo &info)
@@ -431,23 +402,15 @@ Napi::Object OggStreamState::NewInstance(Napi::Value arg)
   return obj;
 }
 
-// NAN_METHOD(node_ogg_stream_init)
-// {
-//   Nan::HandleScope scope;
-//   ogg_stream_state *os = reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0]));
-//   int serialno = static_cast<int>(info[1]->IntegerValue(v8::Isolate::GetCurrent()->GetCurrentContext()).ToChecked());
-//   info.GetReturnValue().Set(Nan::New<Integer>(ogg_stream_init(os, serialno)));
-// }
-
 /* Writes a `ogg_page` struct into a `ogg_stream_state`. */
 class OggStreamPageinWorker : public Napi::AsyncWorker
 {
 public:
-  OggStreamPageinWorker(ogg_stream_state *os, Napi::Function &callback)
-      : Napi::AsyncWorker(callback), os(os), page(), rtn(0) {}
+  OggStreamPageinWorker(ogg_stream_state *os, ogg_page *page, Napi::Function &callback)
+      : Napi::AsyncWorker(callback), os(os), page(page), rtn(0) {}
   void Execute()
   {
-    rtn = ogg_stream_pagein(os, &page);
+    rtn = ogg_stream_pagein(os, page);
   }
 
   void OnOK()
@@ -459,7 +422,7 @@ public:
 
 private:
   ogg_stream_state *os;
-  ogg_page page;
+  ogg_page *page;
   int rtn;
 };
 
@@ -467,20 +430,11 @@ void node_ogg_stream_pagein(const Napi::CallbackInfo &info)
 {
   OggStreamState *streamState =
       Napi::ObjectWrap<OggStreamState>::Unwrap(info[0].As<Napi::Object>());
-  Napi::Function cb = info[1].As<Napi::Function>();
-  (new OggStreamPageinWorker(&streamState->os, cb))->Queue();
+  OggPage *page = Napi::ObjectWrap<OggPage>::Unwrap(info[1].As<Napi::Object>());
+
+  Napi::Function cb = info[2].As<Napi::Function>();
+  (new OggStreamPageinWorker(&streamState->os, &page->op, cb))->Queue();
 }
-
-// NAN_METHOD(node_ogg_stream_pagein)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_stream_state *os = reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0]));
-//   ogg_page *page = reinterpret_cast<ogg_page *>(UnwrapPointer(info[1]));
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(new OggStreamPageinWorker(os, page, callback));
-// }
 
 /* Reads a `ogg_packet` struct from a `ogg_stream_state`. */
 class OggStreamPacketoutWorker : public Napi::AsyncWorker
@@ -532,17 +486,6 @@ void node_ogg_stream_packetout(const Napi::CallbackInfo &info)
   (new OggStreamPacketoutWorker(&streamState->os, &oggPacket->op, cb))->Queue();
 }
 
-// NAN_METHOD(node_ogg_stream_packetout)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_stream_state *os = reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0]));
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[1]));
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(new OggStreamPacketoutWorker(os, packet, callback));
-// }
-
 /* Writes a `ogg_packet` struct to a `ogg_stream_state`. */
 class OggStreamPacketinWorker : public Napi::AsyncWorker
 {
@@ -578,17 +521,6 @@ void node_ogg_stream_packetin(const Napi::CallbackInfo &info)
   Napi::Function cb = info[2].As<Napi::Function>();
   (new OggStreamPacketinWorker(&streamState->os, &oggPacket->op, cb))->Queue();
 }
-
-// NAN_METHOD(node_ogg_stream_packetin)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_stream_state *os = reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0]));
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[1]));
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(new OggStreamPacketinWorker(os, packet, callback));
-// }
 
 // Since both StreamPageout and StreamFlush have the same HandleOKCallback,
 // this base class deals with both.
@@ -647,19 +579,8 @@ void node_ogg_stream_pageout(const Napi::CallbackInfo &info)
        cb))
       ->Queue();
 }
+
 /* Reads out a `ogg_page` struct from an `ogg_stream_state`. */
-// NAN_METHOD(node_ogg_stream_pageout)
-// {
-//   Nan::HandleScope scope;
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(
-//       new StreamPageoutWorker(
-//           reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0])),
-//           reinterpret_cast<ogg_page *>(UnwrapPointer(info[1])),
-//           callback));
-// }
-
 class StreamFlushWorker : public StreamWorker
 {
 public:
@@ -687,138 +608,7 @@ void node_ogg_stream_flush(const Napi::CallbackInfo &info)
       ->Queue();
 }
 
-// NAN_METHOD(node_ogg_stream_flush)
-// {
-//   Nan::HandleScope scope;
-//   Nan::Callback *callback = new Nan::Callback(info[2].As<Function>());
-
-//   Nan::AsyncQueueWorker(
-//       new StreamFlushWorker(
-//           reinterpret_cast<ogg_stream_state *>(UnwrapPointer(info[0])),
-//           reinterpret_cast<ogg_page *>(UnwrapPointer(info[1])),
-//           callback));
-// }
-
-/* Converts an `ogg_page` instance to a node Buffer instance */
-// NAN_METHOD(node_ogg_page_to_buffer)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_page *op = reinterpret_cast<ogg_page *>(UnwrapPointer(info[0]));
-//   unsigned char *buf = reinterpret_cast<unsigned char *>(UnwrapPointer(info[1]));
-//   memcpy(buf, op->header, op->header_len);
-//   memcpy(buf + op->header_len, op->body, op->body_len);
-// }
-
-/* packet->packet = ... */
-// NAN_METHOD(node_ogg_packet_set_packet)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = UnwrapPointer<ogg_packet *>(info[0]);
-//   packet->packet = UnwrapPointer<unsigned char *>(info[1]);
-// }
-
-/* packet->packet */
-// NAN_METHOD(node_ogg_packet_get_packet)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   Nan::MaybeLocal<v8::Object> wrapper = WrapPointer(packet->packet, packet->bytes);
-//   info.GetReturnValue().Set(wrapper.ToLocalChecked());
-// }
-
-/* packet->bytes */
-// NAN_METHOD(node_ogg_packet_bytes)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   info.GetReturnValue().Set(Nan::New<Number>(packet->bytes));
-// }
-
-/* packet->b_o_s */
-// NAN_METHOD(node_ogg_packet_b_o_s)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   info.GetReturnValue().Set(Nan::New<Number>(packet->b_o_s));
-// }
-
-/* packet->e_o_s */
-// NAN_METHOD(node_ogg_packet_e_o_s)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   info.GetReturnValue().Set(Nan::New<Number>(packet->e_o_s));
-// }
-
-/* packet->granulepos */
-// NAN_METHOD(node_ogg_packet_granulepos)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   info.GetReturnValue().Set(Nan::New<Number>(static_cast<double>(packet->granulepos)));
-// }
-
-/* packet->packetno */
-// NAN_METHOD(node_ogg_packet_packetno)
-// {
-//   Nan::HandleScope scope;
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   info.GetReturnValue().Set(Nan::New<Number>(static_cast<double>(packet->packetno)));
-// }
-
-/* Replaces the `ogg_packet` "packet" pointer with a Node.js buffer instance */
-// NAN_METHOD(node_ogg_packet_replace_buffer)
-// {
-//   Nan::HandleScope scope;
-
-//   ogg_packet *packet = reinterpret_cast<ogg_packet *>(UnwrapPointer(info[0]));
-//   unsigned char *buf = reinterpret_cast<unsigned char *>(UnwrapPointer(info[1]));
-//   memcpy(buf, packet->packet, packet->bytes);
-//   packet->packet = buf;
-// }
-
-// NAN_MODULE_INIT(Initialize)
-// {
-//   Nan::HandleScope scope;
-
-//   /* sizeof's */
-// #define SIZEOF(value)                                                        \
-//   Nan::ForceSet(target, Nan::New<String>("sizeof_" #value).ToLocalChecked(), \
-//                 Nan::New<Integer>(static_cast<int32_t>(sizeof(value))),      \
-//                 static_cast<PropertyAttribute>(ReadOnly | DontDelete))
-//   SIZEOF(ogg_sync_state);
-//   SIZEOF(ogg_stream_state);
-//   SIZEOF(ogg_page);
-//   SIZEOF(ogg_packet);
-
-//   Nan::SetMethod(target, "ogg_sync_init", node_ogg_sync_init);
-//   Nan::SetMethod(target, "ogg_sync_write", node_ogg_sync_write);
-//   Nan::SetMethod(target, "ogg_sync_pageout", node_ogg_sync_pageout);
-
-//   Nan::SetMethod(target, "ogg_stream_init", node_ogg_stream_init);
-//   Nan::SetMethod(target, "ogg_stream_pagein", node_ogg_stream_pagein);
-//   Nan::SetMethod(target, "ogg_stream_packetout", node_ogg_stream_packetout);
-//   Nan::SetMethod(target, "ogg_stream_packetin", node_ogg_stream_packetin);
-//   Nan::SetMethod(target, "ogg_stream_pageout", node_ogg_stream_pageout);
-//   Nan::SetMethod(target, "ogg_stream_flush", node_ogg_stream_flush);
-
-//   /* custom functions */
-//   Nan::SetMethod(target, "ogg_page_to_buffer", node_ogg_page_to_buffer);
-
-//   Nan::SetMethod(target, "ogg_packet_set_packet", node_ogg_packet_set_packet);
-//   Nan::SetMethod(target, "ogg_packet_get_packet", node_ogg_packet_get_packet);
-//   Nan::SetMethod(target, "ogg_packet_bytes", node_ogg_packet_bytes);
-//   Nan::SetMethod(target, "ogg_packet_b_o_s", node_ogg_packet_b_o_s);
-//   Nan::SetMethod(target, "ogg_packet_e_o_s", node_ogg_packet_e_o_s);
-//   Nan::SetMethod(target, "ogg_packet_granulepos", node_ogg_packet_granulepos);
-//   Nan::SetMethod(target, "ogg_packet_packetno", node_ogg_packet_packetno);
-//   Nan::SetMethod(target, "ogg_packet_replace_buffer", node_ogg_packet_replace_buffer);
-// }
-
 } // namespace nodeogg
-
-// NODE_MODULE(ogg, nodeogg::Initialize)
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
@@ -828,28 +618,14 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
   OggPage::Init(env, exports);
   OggPacket::Init(env, exports);
 
-  // exports.Set(Napi::String::New(env, "ogg_sync_init"), Napi::Function::New(env, node_ogg_sync_init));
   exports.Set(Napi::String::New(env, "ogg_sync_write"), Napi::Function::New(env, node_ogg_sync_write));
   exports.Set(Napi::String::New(env, "ogg_sync_pageout"), Napi::Function::New(env, node_ogg_sync_pageout));
 
-  // exports.Set(Napi::String::New(env, "ogg_stream_init"), Napi::Function::New(env, node_ogg_stream_init));
   exports.Set(Napi::String::New(env, "ogg_stream_pagein"), Napi::Function::New(env, node_ogg_stream_pagein));
   exports.Set(Napi::String::New(env, "ogg_stream_packetout"), Napi::Function::New(env, node_ogg_stream_packetout));
   exports.Set(Napi::String::New(env, "ogg_stream_packetin"), Napi::Function::New(env, node_ogg_stream_packetin));
   exports.Set(Napi::String::New(env, "ogg_stream_pageout"), Napi::Function::New(env, node_ogg_stream_pageout));
   exports.Set(Napi::String::New(env, "ogg_stream_flush"), Napi::Function::New(env, node_ogg_stream_flush));
-
-  /* custom functions */
-  // exports.Set(Napi::String::New(env, "ogg_page_to_buffer"), Napi::Function::New(env, node_ogg_page_to_buffer));
-
-  // exports.Set(Napi::String::New(env, "ogg_packet_set_packet"), Napi::Function::New(env, node_ogg_packet_set_packet));
-  // exports.Set(Napi::String::New(env, "ogg_packet_get_packet"), Napi::Function::New(env, node_ogg_packet_get_packet));
-  // exports.Set(Napi::String::New(env, "ogg_packet_bytes"), Napi::Function::New(env, node_ogg_packet_bytes));
-  // exports.Set(Napi::String::New(env, "ogg_packet_b_o_s"), Napi::Function::New(env, node_ogg_packet_b_o_s));
-  // exports.Set(Napi::String::New(env, "ogg_packet_e_o_s"), Napi::Function::New(env, node_ogg_packet_e_o_s));
-  // exports.Set(Napi::String::New(env, "ogg_packet_granulepos"), Napi::Function::New(env, node_ogg_packet_granulepos));
-  // exports.Set(Napi::String::New(env, "ogg_packet_packetno"), Napi::Function::New(env, node_ogg_packet_packetno));
-  // exports.Set(Napi::String::New(env, "ogg_packet_replace_buffer"), Napi::Function::New(env, node_ogg_packet_replace_buffer));
 
   return exports;
 }
